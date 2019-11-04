9bd6503fbd05648955f407e1f4051dd10ba6e7c7
ZanderBE <zanderbrownlieedward@gmail.com>
2019-11-04 16:22:49 -0700

    Adding .env file for database access

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const express = require("express"),
       bodyParser = require("body-parser"),
       mongoose = require("mongoose"),
       methodOverride = require("method-override"),
-      expressSanitizer = require("express-sanitizer");
+      expressSanitizer = require("express-sanitizer"),
+      dotenv = require('dotenv').config();
 
 // App Config the for the Dependencies
-// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,4 +18,10 @@ app.use(methodOverride("_method"));
 // Restful Routes
 app.get("/", function(req, res) {
     res.render("/homepage");
-  })
+  });
+
+  // listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+    console.log("Your app is listening on port " + listener.address().port);
+  });
+  
